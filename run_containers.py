ee2d0207e46c2fd11de4cc59954a7a657215cce6
Priya Kasimbeg <kasimbeg@google.com>
2023-04-05 19:09:32 +0000

    update experiment path

--- run_containers.py
+++ run_containers.py
@@ -70,7 +70,7 @@ def main(_):
         print('='*100)
         dataset = WORKLOADS[workload]['dataset']
         max_steps = int(WORKLOADS[workload]['max_steps'] * RUN_FRACTION)
-        experiment_name = f'timing_{algorithm}_v2'
+        experiment_name = f'timing_v2/timing_{algorithm}'
         command = ('docker run -t -d -v /home/kasimbeg/data/:/data/ '
                    '-v /home/kasimbeg/experiment_runs/:/experiment_runs '
                    '-v /home/kasimbeg/experiment_runs/logs:/logs '
