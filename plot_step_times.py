b176bb925e19d96a898237bbcac909116101a349
Priya Kasimbeg <kasimbeg@google.com>
2023-04-07 19:34:39 +0000

    add per phase run times

--- plot_step_times.py
+++ plot_step_times.py
@@ -2,9 +2,11 @@ import log_utils
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 LOG_DIR = 'logs/step_time_logs_v2'
-SAVE_DIR = os.path.join('plots', 'v2')
+SAVE_DIR = os.path.join('plots', 'v2_warmup')
+
 
 def get_workload_name_from_logfilename(logfile):
     filename = os.path.basename(logfile)
@@ -25,7 +27,10 @@ def get_algo_name_from_logfilename(logfile):
     return algo
 
 
-def plot_algo_speeds(logdir=LOG_DIR, framework="jax"):
+def plot_algo_speeds(logdir=LOG_DIR, framework="jax", plot_first_batch=True):
+    if not os.path.exists(SAVE_DIR):
+        os.makedirs(SAVE_DIR)
+
     logfiles = log_utils.get_logfilenames(LOG_DIR)
     logfiles = [f for f in logfiles if framework in f]
 
@@ -40,10 +45,17 @@ def plot_algo_speeds(logdir=LOG_DIR, framework="jax"):
         algo = get_algo_name_from_logfilename(logfile)
 
         run_df = log_utils.extract_results_df(logfile)
-        global_step = run_df['global_step'].iloc[1:].array - run_df['global_step'].iloc[:-1].array
-        total_duration = run_df['total_duration'].iloc[1:].array - run_df['total_duration'].iloc[:-1].array
+        global_step = run_df['global_step'].iloc[1:].to_numpy() - run_df['global_step'].iloc[:-1].to_numpy()
+        total_duration = run_df['total_duration'].iloc[1:].to_numpy()- run_df['total_duration'].iloc[:-1].to_numpy()
         steps_per_sec = global_step / total_duration
-        steps = run_df['global_step'].iloc[1:]
+        steps = run_df['global_step'].iloc[1:].to_numpy()
+        if plot_first_batch == True:
+            steps_per_sec_first_batch = np.array([run_df['global_step'].iloc[0] / run_df['total_duration'].iloc[0]])
+            step_first_batch = np.array([run_df['global_step'].iloc[0]])
+            steps_per_sec = np.hstack((steps_per_sec_first_batch, steps_per_sec))
+            steps = np.hstack((step_first_batch, steps))
+
+        print(type(steps_per_sec))
         # step_time_df.at[algo, workload] = steps_per_second
         step_time_df.at[algo, workload] = {'steps': steps, 'steps_per_sec': steps_per_sec}
 
@@ -62,6 +74,7 @@ def plot_algo_speeds(logdir=LOG_DIR, framework="jax"):
         plt.title(f'{workload}_{framework}')
         plt.ylabel('steps per sec')
         plt.xlabel('step')
+        plt.xticks([x_ for x_ in x ], rotation='vertical')
         plt.legend()
         plt.savefig(os.path.join(SAVE_DIR, f'steps_per_sec_{workload}_{framework}.png'))
 
