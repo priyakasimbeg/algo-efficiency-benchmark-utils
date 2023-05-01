93f5b290bc5f1ea029fd2c481ec83fbb38b1be8c
Priya Kasimbeg <kasimbeg@google.com>
2023-05-01 19:33:24 +0000

    fancy baselines

--- copy_logs.py
+++ copy_logs.py
@@ -1,11 +1,11 @@
 import os
 import log_utils
-root_dir = "/home/kasimbeg/mlcommons-runs/timing_fancy"
-sub_dirs = os.listdir(root_dir)
 
+root_dir = "/home/kasimbeg/mlcommons-runs/timing_fancy"
 destination_dir = "/home/kasimbeg/algo-efficiency-timing/logs/step_time_fancy"
 
-def copy_logs_per_experiment_dir():
+def copy_logs_per_experiment_dir(root_dir=root_dir):
+    sub_dirs = os.listdir(root_dir)
     for algo_dir in sub_dirs:
         if 'timing_' in algo_dir:
             algo = algo_dir.split("_")[1]
@@ -27,17 +27,6 @@ def copy_logs_per_experiment_dir():
                         continue
                     log_file_destination_path = os.path.join(destination_dir, f"{algo}_{log_file}")
                     os.system(f"cp {log_file_source_path} {log_file_destination_path}")
-
-def copy_logs_from_experiment_dir():
-    for experiment_dir in sub_dirs:
-        if 'timing_' in experiment_dir:
-            experiment_path = os.path.join(root_dir, experiment_dir)
-            experiment_dir_contents = os.listdir(experiment_path)
-            log_files = [f for f in experiment_dir_contents if f.endswith(".log")]
-            for log_file in log_files:
-                log_file_source_path = os.path.join(experiment_path, log_file)
-                log_file_destination_path = os.path.join(destination_dir, f'{experiment_dir}')
-                os.system(f"cp {log_file_source_path} {log_file_destination_path}")
 
 if not os.path.exists(destination_dir):
     print(f'Making directory {destination_dir}')
