23cbb7c4d6a97604acb1b6ac1f9b5e9f75489892
Priya Kasimbeg <kasimbeg@google.com>
2023-05-23 05:13:39 +0000

    add debugging script

--- debugging_scripts/run_workloads_pytorch_momentum_timing_no_opt.py
+++ debugging_scripts/run_workloads_pytorch_momentum_timing_no_opt.py
@@ -37,14 +37,14 @@ WORKLOADS = {
             #                             'dataset': 'librispeech'},
             #  'criteo1tb': {'max_steps': 8000,
             #                'dataset': 'criteo1tb'},
-             'librispeech_conformer': {'max_steps': 100000,
-                                       'dataset': 'librispeech'},
+            #  'librispeech_conformer': {'max_steps': 100000,
+            #                            'dataset': 'librispeech'},
 
              }
 
 ALGORITHMS = [
     'momentum',
-    'nesterov'
+    'adamw'
 ]
 
 def container_running():
