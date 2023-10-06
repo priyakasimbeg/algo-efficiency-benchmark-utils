adb2d4b7a765b1b35d920327ca2bbd7f32768cb3
Priya Kasimbeg <kasimbeg@google.com>
2023-10-06 23:48:24 +0000

    cleanup

--- check_targets.py
+++ check_targets.py
@@ -96,62 +96,6 @@ def get_best_metrics_for_all_workloads(experiment_dir):
 
     return pd.DataFrame(best_metric_dict).T
 
-def get_algo_speeds(logdir, framework="jax"):
-
-    # Set up dataframe
-    logfiles = log_utils.get_logfile_paths(LOG_DIR)
-    logfiles = [f for f in logfiles if framework in f]
-
-    workloads = sorted(list(set([get_workload_name_from_logfilename(f) for f in logfiles])))
-    algos = sorted(list(set([get_algo_name_from_logfilename(f) for f in logfiles])))
-
-    columns = []
-    workload_algo = []
-    for workload in workloads:
-        for algo in algos:
-            workload_algo.append(f'{workload}_{algo}')
-
-    df = pd.DataFrame(index=workload_algo, columns=columns)
-
-    # Populate dataframe
-    for logfile in logfiles:
-        workload = get_workload_name_from_logfilename(logfile)
-        algo = get_algo_name_from_logfilename(logfile)
-        index = f'{workload}_{algo}'
-
-        try:
-            run_df = log_utils.extract_results_df(logfile)
-        except ValueError as e:
-            continue
-        print(run_df)
-        df.at[index, 'target setting steps'] = MAX_STEPS[workload]
-        df.at[index, 'baseline steps'] = MAX_STEPS[workload]/0.75
-        df.at[index, 'global step'] = run_df['global_step'].iloc[-1]
-        df.at[index, 'total duration (s)'] = run_df['total_duration'].iloc[-1]
-        df.at[index, 'total eval time (s)'] = run_df['accumulated_eval_time'].iloc[-1]
-        df.at[index, 'total logging & checkpointing_time (s)'] = run_df['accumulated_logging_time'].iloc[-1]
-        try:
-            df.at[index, 'total data selection time (s)'] = run_df['accumulated_data_selection_time'].iloc[-1]
-        except Exception:
-            pass
-        df.at[index, 'total submission time (s)'] = run_df['accumulated_submission_time'].iloc[-1]
-        df.at[index, 'submission time till eval 2 (s)'] = run_df['accumulated_submission_time'].iloc[1]
-        df.at[index, 'num steps till eval 2'] = run_df['global_step'].iloc[1]
-        df.at[index, 'submission time since eval 2 (s)'] = run_df['accumulated_submission_time'].iloc[-1] - run_df['accumulated_submission_time'].iloc[1]
-        df.at[index, 'num steps since eval 2'] = run_df['global_step'].iloc[-1] - run_df['global_step'].iloc[1]
-        df.at[index, 'equilibrium step time (s)'] = df.at[index, 'submission time since eval 2 (s)']/df.at[index, 'num steps since eval 2'] 
-        df.at[index, 'projected max submission time (s)'] = df.at[index, 'submission time till eval 2 (s)'] + df.at[index, 'equilibrium step time (s)'] * (df.at[index, 'baseline steps'] - df.at[index, 'num steps till eval 2'])
-
-    return df
-
-
-# df = get_algo_speeds(framework=FRAMEWORK)
-# print(df)
-# print(df.keys())
-# df.to_csv(os.path.join(OUTPUT_DIR, OUTPUT_FILENAME))
-
-# get_best_metrics('logs/targets_check/jax/criteo1tb_jax_09-14-2023-04-33-24.log')
-
 def main(_):
     if not FLAGS.experiment_log_dir:
         log_dir = f'logs/targets_check/jax'
